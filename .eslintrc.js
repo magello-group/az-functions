84750c6caa2f870640243250871ee23a00f823db
Alvaro Urbano <alle@milboca.com>
2025-01-27 16:28:07 +0100

    adding husky and lint staged

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  ignorePatterns: ['node_modules/', 'dist/', 'build/'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+    },
+  ],
+};
